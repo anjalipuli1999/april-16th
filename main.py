ca3206d374868f509fefdd81e9a4a5836be02f85
anjalipuli2000 <anjalipuli1999@gmail.com>
2024-04-17 03:11:27 +0000

    841

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome you all to Python Flask World v3.0 from cicd -12345'
+    return 'Welcome you all to Python Flask World v3.0 from cicd -12346785'
 
 
 if __name__ == '__main__':
